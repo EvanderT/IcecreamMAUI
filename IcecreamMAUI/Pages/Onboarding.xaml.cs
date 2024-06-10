4be29429569b533e7af75beeeec286cb66c968bf
evander teixeira <evanderlord12@hotmail.com>
2024-06-10 15:36:12 +0100

    page navigation

--- IcecreamMAUI/Pages/Onboarding.xaml.cs
+++ IcecreamMAUI/Pages/Onboarding.xaml.cs
@@ -2,21 +2,13 @@ namespace IcecreamMAUI.Pages;
 
 public partial class Onboarding : ContentPage
 {
-	public Onboarding()
-	{
-		InitializeComponent();
-	}
-
-    private async Task Button_Clicked(object sender, EventArgs e)
+    public Onboarding()
     {
-        try
-        {
-            await Shell.Current.GoToAsync(nameof(HomePage));
-        }
-        catch (Exception ex)
-        {
-            // Aqui você pode adicionar o código para lidar com a exceção, como logging
-            Console.WriteLine($"Erro ao navegar para a HomePage: {ex.Message}");
-        }
+        InitializeComponent();
+    }
+
+    private async void Button_Clicked(object sender, EventArgs e)
+    {
+        await Shell.Current.GoToAsync($"//{nameof(HomePage)}");
     }
 }
